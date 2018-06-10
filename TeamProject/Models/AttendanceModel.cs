65432bcb39d5b443ca357f355f80b73ac2647f70
Daniel Deng <lena@symphoni.cc>
2018-06-10 11:46:22 -0700

    add AttendanceModel

---
+++ TeamProject/Models/AttendanceModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TeamProject.Models
+{
+    /// <summary>
+    /// Pictures for the system
+    /// </summary>
+    public class AttendanceModel
+    {
+        /// <summary>
+        /// ID of the Student
+        /// </summary>
+        public string StudentId { get; set; }
+
+        /// <summary>
+        /// When Logged in
+        /// </summary>
+        public DateTime In { get; set; }
+
+        /// <summary>
+        /// When Logged Out
+        /// </summary>
+        public DateTime Out { get; set; }
+
+        /// <summary>
+        /// Status of the transaction (in, out, hold)
+        /// </summary>
+        public StudentStatusEnum Status { get; set; }
+
+        /// <summary>
+        /// Elapsed time (delta of Out-In)
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+    }
+}
